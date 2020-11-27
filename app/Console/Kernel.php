ed04197fa77faa61c1a949be0b7627c8807085a1
Shipan Mazumder <shipanmazumder@gmail.com>
2020-11-28 01:12:42 +0600

    bot message prod

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:botmessage')->hourly();
+        $schedule->command('command:botmessage')->everyMinute();
     }
 
     /**
