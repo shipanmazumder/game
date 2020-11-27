8f663531d3f9db7332fd6601250a367a045b5f16
Shipan Mazumder <shipanmazumder@gmail.com>
2020-11-28 00:20:49 +0600

    bot message prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <title>Doelcampus</title>
+    <title>404</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Styles -->
