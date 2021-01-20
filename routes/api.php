8cc573e561434b721312316ed10d5b59d6e4caf0
Shipan Mazumder <shipanmazumder@gmail.com>
2021-01-20 21:42:03 +0600

    bot message test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Game;
+use App\Models\BotMessage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\BotController;
 use App\Http\Controllers\API\GameUserController;
@@ -28,3 +31,44 @@ Route::get("/webhook",[BotController::class,'webHookGet']);
 
 Route::post("/delete-user",[GameUserController::class,'deleteUser']);
 Route::get("/deletion",[GameUserController::class,'userDeletion'])->name("userDeletion");
+Route::get("/sendMessage",function(){
+    $games=Game::where("status",1)->latest()->first();
+    $users=DB::table('happy_game_users')
+    ->where("user_unique_id","5438317146193745")
+    ->whereNotNull("sender_id")
+    ->first();
+    $message=BotMessage::where("position",1)->where("status",1)->first();
+    $attachmentMessage=array(
+        "attachment" => array(
+            "type" => "template",
+            "payload" => array(
+                "template_type" => "generic",
+                "elements" => array(
+                    array(
+                        "title" => $message->title,
+                        "image_url" =>$message->image_url,
+                        "subtitle" => $message->subtitle,
+                        "default_action"=>array(
+                            "type"=>"game_play"
+                        ),
+                        "buttons"=>json_decode($message->data)
+                    )
+                )
+            )
+        )
+    );
+    $responData=array(
+        "recipient"=>array(
+            "id"=>$users->sender_psid
+        ),
+        "message"=>$attachmentMessage
+    );
+    $jsonData =json_encode($responData);
+    $url = 'https://graph.facebook.com/v8.0/me/messages?access_token='.$games->game_access_token;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    $result = curl_exec($ch); // user will get the message
+
+});
